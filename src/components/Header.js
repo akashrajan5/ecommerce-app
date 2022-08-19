84b5af5d6ae698d43936f429787d8d5f96161757
aka <akash.perfit@gmail.com>
2022-08-19 19:10:16 +0530

    changed files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {useNavigation} from '@react-navigation/native';
-import {View, Text, StyleSheet, TouchableOpacity} from "react-native";
+import {View, Text, StyleSheet, TouchableOpacity, StatusBar} from "react-native";
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
@@ -9,17 +9,17 @@ export const Header = () => {
   return(
     <View style={styles.header}>
         <TouchableOpacity onPress={() => navigation.openDrawer()}>
-          <MaterialCommunityIcons name="menu" size={27} />{/*onclick change name="menu-open" */}
+          <MaterialCommunityIcons style={styles.iconColor} name="menu" size={27} />{/*onclick change name="menu-open" */}
         </TouchableOpacity>
         <View style={styles.rightView}>
-          <View style={{marginRight: 17}}>
+          <View style={{marginRight: 20}}>
             <TouchableOpacity>
-              <MaterialCommunityIcons name="message-badge" size={24} />
+              <MaterialCommunityIcons style={styles.iconColor} name="message-badge" size={24} />
             </TouchableOpacity>
           </View>
           <View>
             <TouchableOpacity>
-              <MaterialCommunityIcons name="magnify" size={27} />
+              <MaterialCommunityIcons style={styles.iconColor} name="magnify" size={27} />
             </TouchableOpacity>
           </View>
         </View>
@@ -30,15 +30,18 @@ export const Header = () => {
 const styles = StyleSheet.create({
     header: {
       height: hp('6.5%'),
-      backgroundColor: '#ffffff00',
       flexDirection: 'row',
       justifyContent: 'space-between',
       alignItems: 'center',
-      paddingRight: 15,
-      paddingLeft: 12
+      paddingRight: 18,
+      paddingLeft: 12,
+      marginTop: StatusBar.currentHeight
     },
     rightView: {
       flexDirection: 'row',
       alignItems: 'center'
+    },
+    iconColor: {
+      color: '#fff'
     }
 })
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,31 @@
 import React from "react";
-import {View, Text, StyleSheet} from "react-native";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {View, Text, StyleSheet, ImageBackground, StatusBar, TouchableOpacity} from "react-native";
 import {Header} from "../components/Header";
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 export const Home = () => {
   return(
     <>
-      <Header />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent={true} />
+      <ImageBackground source={require('../assets/images/cover.png')} style={styles.coverImageStyle}>
+        <Header />
+        <View style={{flex: 1, marginLeft: 30, marginTop: 40}}>
+          <Text style={styles.coverText}>Make yourself{'\n'}at home</Text>
+        </View>
+      </ImageBackground>
+      <View style={{flexDirection: 'row',justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 14, marginTop: 18,marginBottom: 8}}>
+        <View>
+          <Text style={{fontSize: 30, fontWeight: '600', color: '#000'}}>New Arrivals</Text>
+        </View>
+        <View>
+          <TouchableOpacity style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Text style={{fontSize: 14,fontWeight: '700', color: '#000'}}>Show all</Text>
+            <MaterialCommunityIcons name="menu-right" size={22} />
+          </TouchableOpacity>
+        </View>
+      </View>
+
       <View style={styles.outerView}>
         <Text>Home Page</Text>
       </View>
@@ -21,10 +40,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#c9adff'
   },
-  header: {
-    height: hp('20%'),
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  }
+  coverImageStyle: {
+    width: wp('100%'),
+    height: hp('40%')
+  },
+  coverText: {
+    fontSize: 48,
+    fontWeight: '700',
+    color: '#fff'
+  },
 })
